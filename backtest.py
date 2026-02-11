e8f4b80e00416d845fc780a42b44f8d5dd82db7c
RSKPixel <166299316+RSKPixel@users.noreply.github.com>
2026-02-11 18:15:34 +0530

    udp

---
+++ backtest.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\siven\Desktop\icici-nav.csv")
+
+df['date'] = pd.to_datetime(df['date'])
+df = df.sort_values('date')
+df = df.set_index('date')
+
+yearly = df['nav'].resample('YE').agg({
+    'open': 'first',
+    'high': 'max',
+    'low': 'min',
+    'close': 'last'
+})
+
+df = yearly.copy()
+
+df["change"] = df["close"] - df["open"]
+df["change_percentage"] = df["change"] / df["open"] * 100
+
+df["change_1"] = df["change_percentage"].rolling(window=1).mean()
+df["change_3"] = df["change_percentage"].rolling(window=3).mean()
+df["change_5"] = df["change_percentage"].rolling(window=5).mean()
+df["change_8"] = df["change_percentage"].rolling(window=8).mean()
+df["change_10"] = df["change_percentage"].rolling(window=10).mean()
+
+
+df = df.round(2)
+
+print(df[["change_1", "change_3", "change_5", "change_8", "change_10"]])
